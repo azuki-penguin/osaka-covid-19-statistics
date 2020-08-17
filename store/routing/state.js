74a71e62a6c666de7bab07e434325ccbfa68cae1
azuki-penguin <azukibeans.5kilo@gmail.com>
2020-08-18 02:21:14 +0900

    added route to top page for navigation menu

--- store/routing/state.js
+++ store/routing/state.js
@@ -2,6 +2,12 @@ export default function() {
   return {
     routes: [
       {
+        title: 'トップ',
+        name: 'index',
+        path: '/',
+        component: 'pages/index.vue',
+      },
+      {
         title: '日別の統計情報',
         name: 'daily',
         path: '/daily',
