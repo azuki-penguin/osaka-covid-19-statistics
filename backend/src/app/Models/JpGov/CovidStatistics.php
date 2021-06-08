0514f33f4180f94a603ecd31269409255d71d0dd
azuki-penguin <azukibeans.5kilo@gmail.com>
2021-06-08 22:03:23 +0900

    converted numeric value to integer

--- backend/src/app/Models/JpGov/CovidStatistics.php
+++ backend/src/app/Models/JpGov/CovidStatistics.php
@@ -16,11 +16,11 @@ class CovidStatistics
     {
         return collect($data)
             ->map(function($value, $index) use ($data) {
-                $prevDeathCases = $index > 0 ? $data[$index - 1]['死亡者数'] : 0;
+                $prevDeathCases = $index > 0 ? intval($data[$index - 1]['死亡者数']) : 0;
                 return [
                     'date' => $value['日付'],
-                    'count' => $value['死亡者数'] - $prevDeathCases,
-                    'total' => $value['死亡者数'],
+                    'count' => intval($value['死亡者数']) - $prevDeathCases,
+                    'total' => intval($value['死亡者数']),
                 ];
             });
     }
@@ -37,10 +37,10 @@ class CovidStatistics
         $totalPositiveCases = 0;
         return collect($data)
             ->map(function($value) use (&$totalPositiveCases) {
-                $totalPositiveCases += $value['PCR 検査陽性者数(単日)'];
+                $totalPositiveCases += intval($value['PCR 検査陽性者数(単日)']);
                 return [
                     'date' => $value['日付'],
-                    'count' => $value['PCR 検査陽性者数(単日)'],
+                    'count' => intval($value['PCR 検査陽性者数(単日)']),
                     'total' => $totalPositiveCases,
                 ];
             });
@@ -59,7 +59,7 @@ class CovidStatistics
             ->map(function($value, $index) use ($data) {
                 return [
                     'date' => $value['日付'],
-                    'total' => $value['重症者数'],
+                    'total' => intval($value['重症者数']),
                 ];
             });
     }
@@ -76,10 +76,10 @@ class CovidStatistics
         $totalTestCases = 0;
         return collect($data)
             ->map(function($value) use (&$totalTestCases) {
-                $totalTestCases += $value['PCR 検査実施件数(単日)'];
+                $totalTestCases += intval($value['PCR 検査実施件数(単日)']);
                 return [
                     'date' => $value['日付'],
-                    'count' => $value['PCR 検査実施件数(単日)'],
+                    'count' => intval($value['PCR 検査実施件数(単日)']),
                     'total' => $totalTestCases,
                 ];
             });
